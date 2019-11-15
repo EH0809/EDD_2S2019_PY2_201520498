c0c2a90923571999f37732384a79738f7e158b34
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-11-15 12:23:20 -0600

    Update Pila.java

--- src/Structures/Pila/Pila.java
+++ src/Structures/Pila/Pila.java
@@ -5,10 +5,51 @@
  */
 package Structures.Pila;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author herre
  */
 public class Pila {
+    private NodePila PrimeroPila;
+
+    public Pila() {
+        this.PrimeroPila = null;
+    }
     
+    public boolean Vacia(){
+        return getPrimeroPila() == null;
+    }
+    
+    public void InsertaraPila(String Nombre, String Descripcion){
+        NodePila Nuevo = new NodePila(Nombre, Descripcion,Time());
+        if(Vacia()){
+            setPrimeroPila(Nuevo);
+        }else{
+            Nuevo.setSiguientePila(getPrimeroPila());
+            setPrimeroPila(Nuevo);
+        }
+    }
+    
+    public String Time() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String a = dateFormat.format(new Date());
+        return a;
+    }
+
+    /**
+     * @return the PrimeroPila
+     */
+    public NodePila getPrimeroPila() {
+        return PrimeroPila;
+    }
+
+    /**
+     * @param PrimeroPila the PrimeroPila to set
+     */
+    public void setPrimeroPila(NodePila PrimeroPila) {
+        this.PrimeroPila = PrimeroPila;
+    }
 }
