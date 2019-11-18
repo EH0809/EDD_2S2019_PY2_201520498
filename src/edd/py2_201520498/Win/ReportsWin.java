ad33ef9bbb38dc85ae69b3067bf808cdf549a82e
Herrera _Edgar <53618288+Herrera201520498@users.noreply.github.com>
2019-11-17 23:05:03 -0600

    Update ReportsWin.java

--- src/edd/py2_201520498/Win/ReportsWin.java
+++ src/edd/py2_201520498/Win/ReportsWin.java
@@ -6,6 +6,7 @@
 package edd.py2_201520498.Win;
 
 import edd.py2_201520498.EDDPY2_201520498;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -49,10 +50,25 @@ public class ReportsWin extends javax.swing.JFrame {
         });
 
         jButton2.setText("Graph");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Matrix");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("AVL");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Exit");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -120,12 +136,39 @@ public class ReportsWin extends javax.swing.JFrame {
         // TODO add your handling code here:
         Exit();
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        Grafo ();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        Matriz();
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        AVL();
+    }//GEN-LAST:event_jButton4ActionPerformed
     public void Exit(){
         AdminWin a = new AdminWin();
         a.setVisible(true);
         this.setVisible(false);
     }
-    
+    public void AVL (){
+     String Usuario = JOptionPane.showInputDialog("Ingresar el Nombre del Usuario");
+     String NombreCarpeta = JOptionPane.showInputDialog("Ingresar el Nombre de la Carpeta");
+     Sever.ImprimirAVL(Usuario, NombreCarpeta);
+    }
+    public void Matriz(){
+     String NombreNuevo = JOptionPane.showInputDialog("Ingresar el Nombre del Usuario");
+        Sever.ImprimMatrix(NombreNuevo);
+    }
+    public void Grafo (){
+        String NombreNuevo = JOptionPane.showInputDialog("Ingresar el Nombre del Usuario");
+        Sever.ImprimirGrafo(NombreNuevo);
+    }
     public void Hash(){
         Sever.GraphTable();
     }
